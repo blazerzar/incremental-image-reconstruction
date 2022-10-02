c5733dda8fbda3f268b6eca43716fa6fb6a0c421
Blaž Erzar <blaz.erzar@gmail.com>
2022-10-02 20:04:19 +0200

    Implemented SOR solver

---
+++ src/client/SuccessiveOverRelaxation.ts
@@ -0,0 +1,60 @@
+import { Solver } from "./Solver";
+
+export class SuccessiveOverRelaxation extends Solver {
+    // SOR iteration parameter
+    private omega: number;
+
+    constructor(
+        image: ImageData,
+        size: number,
+        reconstructionCanvas: HTMLCanvasElement,
+        pointsCanvas: HTMLCanvasElement,
+        residualCanvas: HTMLCanvasElement,
+        omega: number
+    ) {
+        super(image, size, reconstructionCanvas, pointsCanvas, residualCanvas, [
+            ["renderToCanvas.vert", "sor/updateRedBlack.frag"],
+        ]);
+        this.omega = omega;
+    }
+
+    protected update(): void {
+        this.updateColor(0);
+        this.updateColor(1);
+    }
+
+    private updateColor(color: 0 | 1): void {
+        this.gl.bindFramebuffer(
+            this.gl.FRAMEBUFFER,
+            this.reconstructionWrite.framebuffer
+        );
+        this.gl.viewport(0, 0, this.size, this.size);
+
+        const { program, uniforms } = this.programs.get("sor/updateRedBlack");
+        this.gl.useProgram(program);
+
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(
+            this.gl.TEXTURE_2D,
+            this.reconstructionRead.texture
+        );
+        this.gl.uniform1i(uniforms.get("uReconstruction"), 0);
+
+        this.gl.activeTexture(this.gl.TEXTURE1);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.pointsDrawing.texture);
+        this.gl.uniform1i(uniforms.get("uPoints"), 1);
+
+        this.gl.uniform1i(uniforms.get("uF"), 2);
+        this.gl.uniform1i(uniforms.get("uRedBlack"), color);
+        this.gl.uniform1f(uniforms.get("uOmega"), this.omega);
+
+        this.gl.bindVertexArray(this.clipVao);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, 4);
+
+        // Swap read and write buffers
+        [this.reconstructionRead, this.reconstructionWrite] = [
+            this.reconstructionWrite,
+            this.reconstructionRead,
+        ];
+    }
+}
---
+++ src/shaders/sor/updateRedBlack.frag
@@ -0,0 +1,42 @@
+#version 300 es
+precision mediump float;
+
+in vec2 vPosition;
+out vec4 oColor;
+
+uniform sampler2D uReconstruction;
+uniform sampler2D uPoints;
+uniform sampler2D uF;
+uniform bool uRedBlack;
+uniform float uOmega;
+
+void main() {
+    vec4 points = texture(uPoints, vPosition);
+    if (points.a >= 1.0) {
+        oColor = points;
+        return;
+    }
+
+    float size = float(textureSize(uReconstruction, 0).x);
+    vec4 c = texture(uReconstruction, vPosition);
+
+    float total = floor(vPosition.x * size) + floor(vPosition.y * size);
+    bool isRed = mod(total, 2.0) == 0.0;
+    if (isRed ^^ uRedBlack) {
+        oColor = c;
+        return;
+    }
+
+    float texelSize = 1.0 / size;
+    float h = 1.0 / (size - 1.0);
+
+    vec4 f = texture(uF, vPosition);
+    vec4 l = texture(uReconstruction, vPosition + vec2(-1,  0) * texelSize);
+    vec4 r = texture(uReconstruction, vPosition + vec2( 1,  0) * texelSize);
+    vec4 d = texture(uReconstruction, vPosition + vec2( 0, -1) * texelSize);
+    vec4 u = texture(uReconstruction, vPosition + vec2( 0,  1) * texelSize);
+    vec4 average = 0.25 * (l + r + d + u - h*h * f);
+
+    oColor = uOmega * average + (1.0 - uOmega) * c;
+}
+
